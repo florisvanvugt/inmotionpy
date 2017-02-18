0da40e7d55dbde2de1d6220c793fe025ab07a912
Floris <f.t.vanvugt@gmail.com>
2017-02-18 15:12:41 -0500

    A rough but working version of native shm management integrating with the robot. A number of sticky issues remain, though.

--- shm_sandbox/shm_native.py
+++ shm_sandbox/shm_native.py
@@ -1,8 +1,26 @@
 
 import sys
 import sysv_ipc
+import struct
 
-OB_KEY = 0x494D5431
+
+
+# Map the type definitions used in the C robdecls.h code
+# to the data types that python's struct uses.
+py_types = {
+    "u32":"I",
+    "f64":"d",
+}
+
+
+
+# Below are the keys that we use to access the shared memory areas
+# where they are located.
+objects = {
+    "Ob"         : 0x494D5431,
+    "Dyncmp_var" : 0x494D5437
+}
+
 #define OB_KEY   0x494D5431
 #define ROB_KEY  0x494D5432
 #define DAQ_KEY  0x494D5433
@@ -12,97 +30,58 @@ OB_KEY = 0x494D5431
 #define DYNCMP_KEY 0x494D5437
 
 
-# Let's create the hash table so we can look up where items are in the shared memory
 
-
-def comment_remover(text):
-    """ Source: http://stackoverflow.com/questions/241327/python-snippet-to-remove-c-and-c-comments """
-    import re
-    def replacer(match):
-        s = match.group(0)
-        if s.startswith('/'):
-            return " " # note: a space and not an empty string
-        else:
-            return s
-    pattern = re.compile(
-        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
-        re.DOTALL | re.MULTILINE
-    )
-    return re.sub(pattern, replacer, text)    
-
-
-
-def parse_robdecls():
+# Read in a table that tells us where each variable is within each object.
+def read_address_probe():
     """ 
-    Now we need to know where C stores its stuff...
+    This reads a table where each row is OB,TYPE,VAR,ADDR
+    where OB is the object of which VAR is a member, and ADDR
+    is the address where that VAR is found, and TYPE is its data type.
+
+    Example line: 
+    Ob u32 mkt_isMcGill 6444
+
+    Returns:
+    A dict where the keys are the variable names (hopefully unique)
+    and the values are tuples (OB,TYPE,ADDR).
     """
-    f = open('../robot/robdecls.h','r')
-    robdecls = f.read()
+    f = open('field_addresses.txt','r')
+    lns = f.readlines()
     f.close()
 
-    purec = comment_remover(robdecls)
-
-
-    td = re.compile(r'typedef\s*struct\s*(\w+)\s*{([^}]*)}\s*(\w+);')
-    matchs = td.findall(txt)
-    structs = {}
-    for match in matchs:
-        obname = match[-1]
-        obcont = match[1]
-
-        contents = [ itm.strip() for itm in obcont.split(";") ]
-
-        vrs = []
-        for cont in contents:
-            if len(cont)>0:
-                fields = cont.split(' ')
-                if len(fields)!=2:
-                    print("Error parsing %s!"%cont)
+    fields = {}
+    for ln in lns:
+        elts = ln.strip().split(" ")
+        if len(elts)==4:
+            ob,tp,var,addr=elts
+            if var in fields:
+                print("WARNING: %s already exist in object."%var)
+            else:
+                if not addr.isdigit():
+                    print("Cannot parse %i as an address (should be an integer)"%addr)
                 else:
-                    vrs.append(fields)
-        structs[obname]=vrs
-    
-    return structs
-
-
-res = parse_robdecls()
-res["xy"]
-
-txt = parse_robdecls()
-
-
-
-# PROBLEM: from the typedef I want to know where in the memory I may find that item... I assume they are in order of appearance but I need to figure out how large each thing is for me to easily find the memory addresses.... now what?
-
-
-
-# Try to attach to the robot memory
-try:
-    ob = sysv_ipc.SharedMemory(OB_KEY) #, [flags = 0, [mode = 0600, [size = 0 or PAGE_SIZE, [init_character = ' ']]]])
-except:
-    print("Cannot access shared memory key OB_KEY. Robot process not running?")
-    sys.exit(-1)
+                    fields[var] = (ob,tp,int(addr))
+        else:
+            print("Ignoring line %s"%ln)
+                
+    return fields
 
 
 
 
-# Map the type definitions used in the C robdecls.h code
-# to stuff we can do in Python
-py_types = {"u32":"I"}
 
-import struct
 
-def read(what,loc):
+def read(memobj,what,loc):
     """ Read from the shared memory.
 
     Arguments
+    memobj : the shared memory object
     what : data type to be read, e.g. "u32". 
     loc : location in the shared memory (offset)
     """
     tp = py_types[what]
     sz = struct.calcsize(tp)
-    # Access something in the shared memory
-    res = ob.read(byte_count=sz,offset=loc)
+    res = memobj.read(byte_count=sz,offset=loc)
 
     return struct.unpack(tp,res)
 
@@ -111,7 +90,7 @@ def read(what,loc):
 # memloc = 6448
 
 
-def write(what,loc,value):
+def write(memobj,what,loc,value):
     """ Write something to the shared memory. 
     
     Arguments
@@ -121,7 +100,7 @@ def write(what,loc,value):
     """
     tp = py_types[what]
     dt = struct.pack(tp,value)
-    ob.write(dt,offset=loc)
+    memobj.write(dt,offset=loc)
     return
     
 
@@ -130,75 +109,67 @@ def write(what,loc,value):
 
 def rshm(var):
     """ Read from shared memory. """
-    loc  = get_location(var)
-    what = get_type(var)
-
-    return read(what,loc)[0]
+    ob,what,loc  = get_info(var)
+    global memobjects
+    return read(memobjects[ob],what,loc)[0]
     
+
 
 def wshm(var,value):
     """ Write to the shared memory. """
-    loc  = get_location(var)
-    what = get_type(var)
-
-    write(what,loc,value)
-
-
-
-
-locs = {"fvv_trial_phase":6448}
-
-
-def get_location(var):
-    return locs[var]
+    ob,what,loc  = get_info(var)
+    global memobjects
+    write(memobjects[ob],what,loc,value)
 
 
 
 
-
-
-
-
-def parse_cmdlist():
-    cmds = {}
-    f = open('cmdlist.txt','r')
-    i = 0
-    for ln in f.readlines():
-        ln = ln.strip()
-        elts = ln.split(' ')
-        name = elts[1]
-        cmds[name]={"index":i,
-                    "type":elts[0],
-                    "loc":elts[2],
-                    "size":elts[3]}
-        i+=1
-    f.close()
-
-    return cmds
+def get_info(var):
+    # Return the OB,TYPE,ADDR for the given variable.
+    global locs
+    if var in locs:
+        return locs[var]
+    else:
+        print("Variable %s not found!"%var)
+        return None
 
 
 
 
 
+def init():
 
-cmds = parse_cmdlist()
+    global locs
+    global memobjects
+    
+    # Read address information about the data
+    locs = read_address_probe()
+
+    memobjects = {}
+
+    for ob in objects:
+
+        # Try to attach to the robot memory
+        try:
+            memobj = sysv_ipc.SharedMemory(objects[ob]) #, [flags = 0, [mode = 0600, [size = 0 or PAGE_SIZE, [init_character = ' ']]]])
+            memobjects[ob]= memobj
+        except:
+            print("Cannot access shared memory key OB_KEY. Robot process not running?")
+            sys.exit(-1)
 
 
-def get_type(var):
-    """ Get the data type for this variable. """
-    return cmds[var]["type"]
 
 
-def gethindex(key):
-    """ Given a key, return its index (in the shared memory). """
-    return cmds[key]
+def quit():
+    # When you are done...    
+
+    global memobjects
+
+    for ob in memobjects:
+        ob.detach()
+    
+            
 
 
-
-
-# When you are done...    
-ob.detach()
-
-
-
-
+        
+init()
